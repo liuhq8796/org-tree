1dfd6ed3ef02079d9e4a7f02ee8433bcde67c1c2
Lucas Liu <100069204+liuhq8796@users.noreply.github.com>
2023-12-13 11:10:31 +0000

    chore: 代码格式化

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-import { resolve } from 'node:path';
+import { resolve } from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -13,8 +13,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   build: {
     lib: {
