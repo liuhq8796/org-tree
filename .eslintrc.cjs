d56b25e16217da13d64994fb7e8de96bde188826
Lucas Liu <100069204+liuhq8796@users.noreply.github.com>
2023-12-13 11:08:41 +0000

    chore: 安装并配置 @stylistic/eslint-plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,8 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript'
+    '@vue/eslint-config-typescript',
+    'plugin:@stylistic/recommended-extends',
   ],
   parserOptions: {
     ecmaVersion: 'latest'
